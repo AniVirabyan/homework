4718abe70d10a53155e50158ed7c8302cc1279c8
AniVirabyan <anivirabyan13@gmail.com>
2024-12-19 20:29:40 +0400

    added main function

--- joke.py
+++ joke.py
@@ -6,24 +6,35 @@ def get_joke():
         return response.json()
     else:
         return None
-evaluations = []
-for i in range(10):     
-    joke_data = get_joke()
-    
-    if joke_data:
-        setup = joke_data['setup']
-        punchline = joke_data['punchline']
-        
-        print("\nJoke " + str(i+1) + " from  10:")
-        try:
-            evaluation = int(input("Rate the joke : "))
-            if 1 <= evaluation <= 10:
-                evaluations.append(evaluation)
-            else:
-                print("from 1 to 10")
-        except ValueError:
-            print("Number Only")
-    
+def main():
+    evaluations = []
+    jokes=[]   
+    for i in range(10):     
+        joke_data = get_joke()        
+        if joke_data:
+            setup = joke_data['setup']
+            punchline = joke_data['punchline'] 
+                     
+            print("\nJoke " + str(i+1) + " from  10:")
+            while True:
+                try:
+                    evaluation = int(input("Rate the joke : "))
+                    if 1 <= evaluation <= 10:
+                        evaluations.append(evaluation)
+                        break
+                    else:
+                        print("from 1 to 10")
+                except ValueError:
+                    print("Number Only")
+    with open("jokes.txt","w") as file:
+        for i in range(len(jokes)):
+            file.write("joke"+str(i+1)+"\n")
+            file.write("Rating"+str(evaluations[i])+"\n")
+        print("All jokes and ratings have been saved to 'jokes.txt'.")
+if __name__=="__main__":
+    main()
+
+            
 
 
 
